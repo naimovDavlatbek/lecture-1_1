ba130e4d6dd2cdd775be006cf356dce843bef61a
Davlatbek <davlatbeknaimov@810gmail.com>
2024-03-12 14:34:43 +0300

    s

---
+++ index.js
@@ -0,0 +1,138 @@
+// //1
+//     function max(n1,n2) {
+//         if(n1>n2){
+//             return n1;
+//         }
+//         else{
+//             return n2;
+//         }
+        
+//     }
+//     console.log(max(85,281));
+// let n1=5;
+// let n2=8454;
+//   let res = n1>n2 ? n1 : n2;
+//   console.log(res);
+
+///2
+//   function chan(num) {
+//     if(num>0){
+//         return 1;
+//     }
+//     if(num<0){
+//         return -1;
+//     }
+//     return 0;
+    
+//   }
+//   console.log(chan(0));
+
+//3
+//    function name(num) {
+//       if(num>0){
+//         return -num
+//       }
+//       else if(num<0) {
+//         return Math.abs(num)
+//       }
+//       else{
+//         return 0;
+//       }
+    
+//    }
+//    console.log(name(0));
+   
+
+// ///4
+//    function posnegzero(arr) {
+//     let pos=0;
+//     let neg=0;
+//     let zero = 0;
+//     for(let i=0; i<arr.length; i++){
+//     if(arr[i]>0){
+//          pos++;
+//     }
+//     else if(arr[i]<0){
+//          neg++;
+//     }
+//        else{
+//             zero++;
+//        }
+//     }
+    
+//       return{
+//         positive:pos,
+//         negative: neg,
+//         zero: zero,
+//       }
+      
+//    }
+//    console.log(posnegzero(15,27,-45));
+
+//5
+    // function butun(n1,n2,n3) {
+    //     if(n1===n2 && n2===n3){
+    //         return 3;
+    //     }
+    //     else if(n1==n2 || n1===n3 || n2===n3){
+    //         return 2;
+    //     }
+    //     else{
+    //         return 0;
+    //     }
+        
+    // }
+    // console.log(butun(1,2,2));
+
+    //6
+    // function loop(n) {
+    //     for(let i=0; i<=n; i++){
+    //         console.log(i);
+    //   }    
+    // }
+    // let n=7;
+    //  loop(n);
+
+
+    //7
+    //   function printtwo(n1,n2) {
+    //     for(let i=n1; i<=n2; i++){
+    //     console.log(i);
+    //     }
+    //   }
+    //   let n1=15;
+    //   let n2=25;
+    //   printtwo(n1,n2)
+
+
+    //8
+          
+    // let a = 4;
+    // let b= 10;
+    // for (let i = a; i <= b; i++) {
+    //     if (i % 2 !== 0) {
+    //         console.log(i);
+    //     }
+    // }
+
+    //9
+    // function print(n) {
+    //     for(let i=0; i<=n; i++){
+    //         console.log(i+" Hello World");
+    //     }
+        
+    // }
+    // let n=8;
+    // print(n)
+
+
+//10
+
+    // function sum(n) {
+    //     let cnt=0;
+    //     for(let i=0; i<=n; i++){
+    //         cnt+=i;
+    //     }
+    //     return cnt;
+    // }
+    // console.log(sum(5));
